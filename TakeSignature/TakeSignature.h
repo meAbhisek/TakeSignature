08d047123e34662945670ed7574dbe0a3e29a008
User-81-Mac <user-81-mac@user-81-macs-mac-mini.local>
2016-01-13 12:56:25 +0530

    Initial Commit

---
+++ TakeSignature/TakeSignature.h
@@ -0,0 +1,13 @@
+//
+//  TakeSignature.h
+//  TakeSignature
+//
+//  Created by User-81-Mac on 13/01/16.
+//  Copyright © 2016 User-81-Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TakeSignature : NSObject
+
+@end
---
+++ TakeSignature/TakeSignature.m
@@ -0,0 +1,13 @@
+//
+//  TakeSignature.m
+//  TakeSignature
+//
+//  Created by User-81-Mac on 13/01/16.
+//  Copyright © 2016 User-81-Mac. All rights reserved.
+//
+
+#import "TakeSignature.h"
+
+@implementation TakeSignature
+
+@end
